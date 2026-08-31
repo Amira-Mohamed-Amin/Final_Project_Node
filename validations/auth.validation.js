15aa65e2079003669a68830087745b45da7b98e3
Amira Mohamed <am5330599@gmail.com>
2026-09-01 02:15:15 +0300

    modified validation and index

--- validations/auth.validation.js
+++ validations/auth.validation.js
@@ -1,12 +1,16 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  body: Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  }).required(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }).required(),
 });
